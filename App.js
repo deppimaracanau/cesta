1441126458b84cbcb6e7f686eae6e90be529ce15
deppimaracanau <50217555+deppimaracanau@users.noreply.github.com>
2022-02-08 16:39:17 -0300

    styles and fonts
    Adicionada fontes ao app e dando uma "estilizada"

--- App.js
+++ App.js
@@ -1,9 +1,24 @@
 import React from 'react'; 
-import { StatusBar, SafeAreaView} from 'react-native';
+import { StatusBar, SafeAreaView, View} from 'react-native';
+import { useFonts, 
+  Montserrat_400Regular, 
+  Montserrat_700Bold } 
+  from 'expo-google-fonts/motserrat';
 
 import  Cesta from './src/telas/Cesta';
+import { View } from 'react-native-web';
 
 export default function App() {
+
+  const[Fontecarregada] = useFonts({
+    "MontserratRegular": Montserrat_400Regular,
+    "MontserratBold": Montserrat_700Bold,
+  });
+
+  if  (!Fontecarregada){
+    return <View />
+  }
+
   return (
     <SafeAreaView>
       <StatusBar />
--- src/telas/Cesta.js
+++ src/telas/Cesta.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { StyleSheet, Image, Dimensions, Text } from "react-native";
+import { StyleSheet, Image, Dimensions, Text, View } from "react-native";
 
-import topo from '../../assets/';
+import topo from '../../assets/topo.png';
+import logo from '../../assets/logo.png';
 
 const width = Dimensions.get('screen').width
 
@@ -9,6 +10,19 @@ export default function Cesta(){
     return <>
         <Image source={topo} Style={estilos.topo}/> 
         <Text style={estilos.titulo}>Detalhes da cesta</Text>
+
+        <view style={estilos.cesta}>
+            <Text stylo={estilos.nome}>Cesta de legumes </Text>
+            <view style={estilos.fazenda}>
+            <Text style={estilos.nomeFazenda}>Fazenda do chico doido</Text>
+            </view>
+            <Image source={logo} style={estilos.imagemFazenda}/>
+            <Text style={estilos.descrição}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+            when an unknown printer took a galley of type and scrambled it to make a type specimen book
+            </Text>
+            <Text style={estilos.preco}>R$ 40,00</Text>
+        </view>
         </>
 }
 
@@ -26,7 +40,45 @@ const estilos = StyleSheet.create({
         lineHeight: 26,
         color: "white",
         fontWeight: "bold",
-        padding: 16
+        padding: 16,
 
-    }
+    },
+    cesta: {
+        paddingHorizontal: 8,
+        paddingHorizontal: 16,
+    },
+    nome:{
+        color: "#464646",
+        fontSize: 26,
+        lineHeight: 42,
+        fontWeight: "bold",
+        fontFamily: "MotserratBold",
+    },
+    fazenda:{
+        flexDirection: "row",
+        paddingVertical: 12,
+    },
+    imagemFazenda:{
+        width: 32,
+        height: 32,
+        marginLeft: 12,
+    },
+    nomeFazenda:{
+     fontSize: 16,
+     lineHeight: 26, 
+     fontFamily: "MotserratRegular", 
+
+    },
+    descrição:{
+        color: "#a3a3a3",
+        fontSize: 16,
+        lineHeight: 26,
+    },
+    preco: {
+        color: "#2a9f85",
+        fontWeight: "bold",
+        fontSize: 26,
+        lineHeight: 42,
+        marginTop: 8,
+    },
 });
