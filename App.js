570c83893b74444bdad6cefe3ab02e247d3afc94
deppimaracanau <50217555+deppimaracanau@users.noreply.github.com>
2022-02-10 08:27:06 -0300

    segunda parte do curso
    Refatorando o app,colocando mais componentes.

--- App.js
+++ App.js
@@ -1,28 +1,29 @@
-import React from 'react'; 
-import { StatusBar, SafeAreaView, View} from 'react-native';
-import { useFonts, 
+import React from 'react';
+import { StatusBar, SafeAreaView, View } from 'react-native';
+import { 
+  useFonts, 
   Montserrat_400Regular, 
-  Montserrat_700Bold } 
-  from 'expo-google-fonts/motserrat';
+  Montserrat_700Bold,
+} from '@expo-google-fonts/montserrat';
+import AppLoading from 'expo-app-loading';
 
-import  Cesta from './src/telas/Cesta';
-import { View } from 'react-native-web';
+import Cesta from './src/telas/Cesta';
+import mock from './src/mocks/cesta';
 
 export default function App() {
-
-  const[Fontecarregada] = useFonts({
+  const [fonteCarregada] = useFonts({
     "MontserratRegular": Montserrat_400Regular,
     "MontserratBold": Montserrat_700Bold,
   });
 
-  if  (!Fontecarregada){
-    return <View />
+  if (!fonteCarregada) {
+    return <AppLoading />
   }
 
   return (
     <SafeAreaView>
       <StatusBar />
-      <Cesta />
+      <Cesta {...mock} />
     </SafeAreaView>
   );
 }
